6b18848f5307e19fe1ab022d2d8f7d150b9423f8
sohailarshaddev <sohailarshad.dev@gmail.com>
2026-04-01 11:56:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = "";
+}
+``
